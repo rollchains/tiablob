8e21531e05cb6b4a02c5bbb4f6a0741cacea4205
Andrew Gouin <andrew@gouin.io>
2024-04-23 16:09:34 -0600

    Add app wiring guide

--- relayer/config.go
+++ relayer/config.go
@@ -14,7 +14,39 @@ const (
 	FlagGasAdjustment = "celestia.gas-adjustment"
 	FlagQueryInterval = "celestia.proof-query-interval"
 	FlagMaxFlushSize  = "celestia.max-flush-size"
+
+	DefaultConfigTemplate = `
+
+	[celestia]
+	# RPC URL of celestia-app node for posting block data
+	# TODO remove hardcoded URL
+	rpc-url = "https://rpc-mocha.pops.one:443"
+
+	# RPC Timeout for transaction broadcasts and queries to celestia-app node
+	rpc-timeout = "30s"
+
+	# Gas price to pay for celestia transactions
+	gas-prices = "0.01utia"
+
+	# Gas adjustment for celestia transactions
+	gas-adjustment = 1.0
+
+	# Query celestia for new block proofs this often
+	proof-query-interval = "12s"
+
+	# Only flush at most this many block proofs in an injected tx per block proposal
+	max-flush-size = 32
+	`
 )
+
+var DefaultCelestiaConfig = CelestiaConfig{
+	RpcURL:             "https://rpc-mocha.pops.one:443", // TODO remove hardcoded URL
+	RpcTimeout:         30 * time.Second,
+	GasPrice:           "0.01utia",
+	GasAdjustment:      1.0,
+	ProofQueryInterval: 12 * time.Second,
+	MaxFlushSize:       32,
+}
 
 // CelestiaConfig defines the configuration for the in-process Celestia relayer.
 type CelestiaConfig struct {
