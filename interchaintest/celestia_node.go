3063be3fdb53a0818394b8f9085b1b971d8d64da
Steve <1848680+misko9@users.noreply.github.com>
2024-04-25 14:32:45 -0600

    Interchaintest for publishing blobs (#3)
    * Interchaintest for publishing blobs
    
    * Add celestia-node support
    
    * ensure images are pulled, bump celestia node and ictest
    
    ---------
    
    Co-authored-by: Andrew Gouin <andrew@gouin.io>

---
+++ interchaintest/celestia_node.go
@@ -0,0 +1,58 @@
+package e2e
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
+	"go.uber.org/zap"
+)
+
+type CelestiaNodeClient struct {
+	log *zap.Logger
+
+	sc *cosmos.SidecarProcess
+	nodeStore string
+}
+
+func NewCelestiaNodeClient(log *zap.Logger, sc *cosmos.SidecarProcess, nodeStore string) *CelestiaNodeClient {
+	return &CelestiaNodeClient{
+		log: log,
+		sc: sc,
+		nodeStore: nodeStore,
+	}
+}
+
+func (n *CelestiaNodeClient) GetAllBlobs(ctx context.Context, height uint64, namespace string) ([]jsonBlob, error) {
+	cmd := []string{
+		"celestia", "blob", "get-all", fmt.Sprint(height), namespace,
+		"--node.store", n.nodeStore,
+		"--url", fmt.Sprintf("http://%s:26658", n.sc.HostName()),
+		"--base64",
+	}
+	stdout, _, err := n.sc.Exec(ctx, cmd, []string{})
+	if err != nil {
+		return nil, err
+	}
+	
+	var blobs jsonGetAllBlobs
+	err = json.Unmarshal(stdout, &blobs)
+	if err != nil {
+		return []jsonBlob{}, nil
+	}
+
+	return blobs.Blobs, nil
+}
+
+type jsonGetAllBlobs struct {
+	Blobs []jsonBlob `json:"result"`
+}
+
+type jsonBlob struct {
+	Namespace    []byte `json:"namespace"`
+	Data         []byte `json:"data"`
+	ShareVersion uint32 `json:"share_version"`
+	Commitment   []byte `json:"commitment"`
+	Index        int    `json:"index"`
+}
---
+++ interchaintest/publish_test.go
@@ -0,0 +1,149 @@
+package e2e
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"testing"
+
+	"cosmossdk.io/math"
+	"github.com/strangelove-ventures/interchaintest/v8"
+	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/strangelove-ventures/interchaintest/v8/testutil"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+
+	blocktypes "github.com/cometbft/cometbft/proto/tendermint/types"
+)
+
+func TestPublish(t *testing.T) {
+	celestiaAppHostname := fmt.Sprintf("%s-val-0-%s", celestiaChainID, t.Name()) // celestia-1-val-0-TestPublish
+	//celestiaNodeHostname := fmt.Sprintf("%s-celestia-node-0-%s", celestiaChainID, t.Name()) // celestia-1-celestia-node-0-TestPublish
+
+	rollchainChainSpec := DefaultChainSpec //nolint:copylockss
+	nv := 2
+	rollchainChainSpec.NumValidators = &nv
+	rollchainChainSpec.ConfigFileOverrides = testutil.Toml{
+		"config/app.toml": testutil.Toml{
+			"celestia": testutil.Toml{
+				"rpc-url": fmt.Sprintf("http://%s:26657", celestiaAppHostname),
+			},
+		},
+	}
+	celestiaChainSpec := CelestiaChainSpec //nolint:copylockss
+	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+		&rollchainChainSpec,
+		&celestiaChainSpec,
+	})
+
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	rollchainChain := chains[0].(*cosmos.CosmosChain)
+	celestiaChain := chains[1].(*cosmos.CosmosChain)
+
+	ic := interchaintest.NewInterchain().
+		AddChain(rollchainChain).
+		AddChain(celestiaChain, ibc.WalletAmount{
+			Address: "celestia1dr3gwf5kulm4e4k0pctwzn0htw6wrvevdgjdlf",
+			Amount:  math.NewInt(100_000_000_000), // 100,000 tia
+			Denom:   celestiaChainSpec.Denom,
+		})
+
+	ctx := context.Background()
+	client, network := interchaintest.DockerSetup(t)
+
+	require.NoError(t, ic.Build(ctx, nil, interchaintest.InterchainBuildOptions{
+		TestName:         t.Name(),
+		Client:           client,
+		NetworkID:        network,
+		SkipPathCreation: true,
+		//BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
+	}))
+	t.Cleanup(func() {
+		_ = ic.Close()
+	})
+
+	for i := 0; i < nv; i++ {
+		// celestia1dr3gwf5kulm4e4k0pctwzn0htw6wrvevdgjdlf
+		stdout, stderr, err := rollchainChain.Validators[i].ExecBin(ctx, "keys", "tiablob", "restore", "kick raven pave wild outdoor dismiss happy start lunch discover job evil code trim network emerge summer mad army vacant chest birth subject seek")
+		require.NoError(t, err, "stdout: %s, stderr: %s", stdout, stderr)
+		t.Log(string(stdout), string(stderr))
+	}
+
+	fundAmount := math.NewInt(100_000_000)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, "default", fundAmount, rollchainChain, celestiaChain)
+	rollchainUser := users[0]
+	celestiaUser := users[1]
+	err = testutil.WaitForBlocks(ctx, 2, rollchainChain, celestiaChain) // Only waiting 1 block is flaky for parachain
+	require.NoError(t, err, "celestia chain failed to make blocks")
+
+	// Check balances are correct
+	rollchainUserAmount, err := rollchainChain.GetBalance(ctx, rollchainUser.FormattedAddress(), rollchainChain.Config().Denom)
+	require.NoError(t, err)
+	require.True(t, rollchainUserAmount.Equal(fundAmount), "Initial rollchain user amount not expected")
+	celestiaUserAmount, err := celestiaChain.GetBalance(ctx, celestiaUser.FormattedAddress(), celestiaChain.Config().Denom)
+	require.NoError(t, err)
+	require.True(t, celestiaUserAmount.Equal(fundAmount), "Initial celestia user amount not expected")
+
+	celestiaNode := StartCelestiaNode(t, ctx, celestiaChain, client, network)
+
+	celestiaNodeClient := NewCelestiaNodeClient(nil, celestiaNode, celestiaNodeHome)
+
+	success := watchForPublishedBlocks(t, ctx, celestiaNodeClient, rollchainChain, celestiaChain, 50)
+	require.True(t, success, "failed to find all published blocks")
+}
+
+func watchForPublishedBlocks(
+	t *testing.T,
+	ctx context.Context,
+	celestiaNodeClient *CelestiaNodeClient,
+	rollchainChain *cosmos.CosmosChain,
+	celestiaChain *cosmos.CosmosChain,
+	publishedBlockCount int64,
+) bool {
+	// Run test to observe x blocks posted
+	rollchainBlocksSeen := int64(0)
+	rollchainHighestBlock := int64(0)
+	celestiaHeight := int64(1)
+
+	// setup time will allow publishedBlockCount to be used as a timeout
+	for i := int64(0); i < publishedBlockCount; i++ {
+		err := testutil.WaitForBlocks(ctx, 1, rollchainChain)
+		require.NoError(t, err, "failed to wait for 1 block")
+
+		celestiaLatestHeight, err := celestiaChain.Height(ctx)
+		require.NoError(t, err, "error getting celestia height")
+
+		for ; celestiaHeight < celestiaLatestHeight; celestiaHeight++ {
+			blobs, err := celestiaNodeClient.GetAllBlobs(ctx, uint64(celestiaHeight), "0x"+hex.EncodeToString([]byte("rc_demo")))
+			require.NoError(t, err, fmt.Sprintf("error getting all blobs at height: %d, %v", celestiaHeight, err))
+			t.Log("GetAllBlobs, celestia height: ", celestiaHeight)
+			if len(blobs) == 0 {
+				t.Log("No blobs found")
+			} else {
+				for j := 0; j < len(blobs); j++ {
+					var block blocktypes.Block
+					err = block.Unmarshal(blobs[j].Data)
+					if err != nil {
+						t.Log("Error unmarshalling block")
+					} else {
+						rollchainBlocksSeen++
+						if rollchainHighestBlock < block.Header.Height {
+							rollchainHighestBlock = block.Header.Height
+						}
+						t.Log("Block ", block.Header.Height, " found")
+					}
+				}
+			}
+		}
+		// Current expectation is that blocks will not miss being published, this will change once tiablob retry logic is added
+		require.Equal(t, rollchainBlocksSeen, rollchainHighestBlock, "rollchain published blocks missed")
+		if rollchainHighestBlock >= publishedBlockCount {
+			return true
+		}
+	}
+
+	return false
+}
