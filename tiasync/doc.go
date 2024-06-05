3d8afbdf51eb52ac395b72a361a1ce4a0522e536
Steve Miskovetz <steve.miskovetz@gmail.com>
2024-06-05 08:37:44 -0600

    Add details on tiasync

--- tiasync/doc.go
+++ tiasync/doc.go
@@ -1,1 +1,13 @@
 package tiasync
+
+/*
+tiasync is a cometbft p2p proxy.
+When enabled, proper configurations are verified and cometbft's only p2p connection is to tiasync.
+Tiasync's peers are the local cometbft instance and if necessary, at least one upstream peer.
+An upstream peer is necessary if either state sync is used or to propagate transactions.
+The local cometbft instance must not connect to any other peers than tiasync.
+Tiasync is used to manage state sync, block sync, and transaction propagation (mempool).
+Block sync is started at either genesis or after state sync. Once block sync has started, the node
+will stay in this mode, never moving on to consensus. Tiasync will deliver blocks from Celestia to
+the local node using block sync.
+*/
